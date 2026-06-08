ffee36c508c5d02f783e7b44b95c0561fde7bd1d
Ana Marugal <ana.marugal@escola.pr.gov.br>
2026-06-08 13:48:47 +0000

    V2

--- animal.js
+++ animal.js
@@ -6,5 +6,10 @@ const animal = {
     vacinado: true
 };
 
-console.log(animal);
-// console.log(animal.nome);
+console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 6;
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com ${animal.idade} anos de idade e possui sua vacina em dia (${animal.vacinado}).`)
